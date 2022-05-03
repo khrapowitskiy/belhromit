49a5ef37adba4fb51b6e169f0c1f9d6a2aa7e860
khrapowitskiy <neragetaa@gmail.com>
2022-05-03 21:18:24 +0300

    burger

--- js/script.js
+++ js/script.js
@@ -14,7 +14,10 @@ autoplay:{
 	disableOnInteraction: false
 },
 slidesPerView:6,
+
 });
+
+
 
 new Swiper('.image-slider2', {
 	
@@ -33,3 +36,67 @@ navigation:{
 },
 slidesPerView:6,
 });
+
+
+new Swiper('.image-slider4', {
+	
+pagination:{
+	el:'.swiper-pagination',
+	clickable: true,
+},
+autoplay:{
+	delay:3000,
+	stopOnLastSlide:false,
+	disableOnInteraction: false
+},
+navigation:{
+	nextEl: '.swiper-button-next',
+	prevEl: '.swiper-button-prev'
+},
+slidesPerView:4,
+});
+
+new Swiper('.image-slider3', {
+	
+pagination:{
+	el:'.swiper-pagination',
+	clickable: true,
+},
+autoplay:{
+	delay:3000,
+	stopOnLastSlide:false,
+	disableOnInteraction: false
+},
+navigation:{
+	nextEl: '.swiper-button-next',
+	prevEl: '.swiper-button-prev'
+},
+slidesPerView:4,
+});
+
+function burgerMenu(selector) {
+	let menu = $(selector);
+	let button = menu.find('.burger-menu__button');
+	let links = menu.find('.burger-menu__link');
+	let overlay = menu.find('.burger-menu__overlay');
+
+button.on('click', (e) => {
+	e.preventDefault();
+	toggleMenu();
+});
+
+links.on('click', () => toggleMenu());
+overlay.on('click', () => toggleMenu());
+
+	function toggleMenu() {
+		menu.toggleClass('burger-menu_active');
+
+		if (menu.hasClass('burger-menu_active')) {
+			$('body').css();
+		} else {
+				$('body').css('overflow', 'visible');
+		}
+	}
+}
+
+burgerMenu('.burger-menu');
